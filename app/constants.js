0b80f2e9322dce916b0f2a422edcb6015d62c1b7
Mathias Oberson <mathias@squareservices.ch>
2020-10-08 17:25:40 +0200

    Proposed solution

---
+++ app/constants.js
@@ -0,0 +1,19 @@
+/**
+ * ------------------------------------------------------------------------
+ *
+ * This module exports string constants used throughout the application.
+ * It helps to minimize the risk of type by preventing string repetition.
+ *
+ * ------------------------------------------------------------------------
+ */
+export const formEleId = 'register-form';
+export const errorContainerEleId = 'errors';
+export const errorListEleId = 'error-list';
+export const emailVerificationUrl =
+  'https://us-central1-mdt-gio1.cloudfunctions.net/notUsed';
+export const userRegistrationUrl =
+  'https://us-central1-mdt-gio1.cloudfunctions.net/registerUser';
+export const HttpMethods = {
+  GET: 'GET',
+  POST: 'POST'
+};

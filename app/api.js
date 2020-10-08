07273588778da538a96eb75e91398c1c1e8a7a5b
Mathias Oberson <mathias@squareservices.ch>
2020-10-08 17:54:47 +0200

    Use fetch instead of XMLHttpRequest

--- app/api.js
+++ app/api.js
@@ -19,16 +19,13 @@ import User from './user.js';
  * @param {string} email The email to verifiy
  * @return {Promise<boolean>} A Promise whose value will be `true` if the email is already taken and `false` otherwise
  */
-export function isEmailExisting(email) {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open(HttpMethods.GET, `${emailVerificationUrl}?email=${email}`);
-    req.send();
-    req.onload = () => {
-      const result = JSON.parse(req.response);
-      resolve(!result.available);
-    };
+export async function isEmailExisting(email) {
+  const response = await fetch({
+    method: HttpMethods.GET,
+    url: `${emailVerificationUrl}?email=${email}`
   });
+  const result = response.json();
+  return !result.available;
 }
 
 /**
@@ -37,13 +34,11 @@ export function isEmailExisting(email) {
  * @param {RegistrationData} registration The user registration data
  * @returns {Promise<User>} A Promise whose value will be an object representing the created user
  */
-export function sendRegistration(registration) {
-  return new Promise((resolve, reject) => {
-    const req = new XMLHttpRequest();
-    req.open(HttpMethods.POST, userRegistrationUrl);
-    req.onload = () => {
-      resolve(new User(JSON.parse(req.response)));
-    };
-    req.send(JSON.stringify(registration));
+export async function sendRegistration(registration) {
+  const response = await fetch({
+    method: HttpMethods.POST,
+    url: userRegistrationUrl,
+    body: JSON.stringify(registration)
   });
+  return new User(response.json());
 }
